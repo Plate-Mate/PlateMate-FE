24e8ffb6caef8e104fe93ef5c6bcb89229d8bb8f
lee minwoo <lmw1663@gachon.ac.kr>
2024-05-24 11:27:58 +0900

    a

---
+++ app/src/main/java/com/example/platemate/ChoosePeople.java
@@ -0,0 +1,24 @@
+package com.example.platemate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChoosePeople extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_choose_people);
+
+
+
+
+
+    }
+}
